365769fb16e71d566ad95bab5ecde941c0a49155
unknown <2889250797@qq.com>
2020-03-23 19:57:36 +0800

    change package.json

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -68,9 +68,9 @@ export default {
     submitForm(formName) {
       this.$refs[formName].validate(valid => {
         if (valid) {
-          alert("submit!");
+
         } else {
-          console.log("error submit!!");
+          
           return false;
         }
       });
